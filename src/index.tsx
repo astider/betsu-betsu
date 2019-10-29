8dc78b29e294b12c176b72854e40055a2b4d151c
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2019-10-29 17:22:42 +0700

    Add hot reload

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<App />, rootElement);
+
+if ((module as any).hot) {
+  (module as any).hot.accept('./App', () => {
+    const NextApp = require('./App').default; // eslint-disable-line
+    ReactDOM.render(
+      <NextApp />,
+      rootElement,
+    );
+  });
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
